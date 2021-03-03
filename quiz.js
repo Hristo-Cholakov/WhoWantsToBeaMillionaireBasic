baa9636a9d8ba138ab913bd957735fa2e6b9d114
Hristo <hristo_ixa@abv.bg>
2021-03-03 22:59:56 +0200

    Adding IIFE to separate the code as API

--- quiz.js
+++ quiz.js
@@ -1,45 +1,47 @@
-var questionOptions = 65;
-var givenAnswer;
-var position;
-var score = 0;
+(function () {
+  var questionOptions = 65;
+  var givenAnswer;
+  var position;
+  var score = 0;
 
-function questionConstructor(questionItself, answers, correctAnswer){
+  function questionConstructor(questionItself, answers, correctAnswer) {
     this.questionItself = questionItself;
     this.answers = answers;
     this.correctAnswer = correctAnswer;
-};
-var firstQuestion = new questionConstructor("Is js the coolest?", ["Yes", "No", "Perhaps"], "A");
-var secondQuestion = new questionConstructor("Who is your teacher?", ["Me", "You"], "A");
+  };
+  var firstQuestion = new questionConstructor("Is js the coolest?", ["Yes", "No", "Perhaps"], "A");
+  var secondQuestion = new questionConstructor("Who is your teacher?", ["Me", "You"], "A");
 
-var questionArray = [firstQuestion, secondQuestion];
+  var questionArray = [firstQuestion, secondQuestion];
 
-function randomizer(){
-  position = Math.round(Math.random())*1;
-  var counter = questionArray[position].answers.length;
+  function randomizer() {
+    position = Math.round(Math.random()) * 1;
+    var counter = questionArray[position].answers.length;
     console.log(questionArray[position].questionItself);
-    for (var i = 0; i < counter; i++){
+    for (var i = 0; i < counter; i++) {
       var questionLetter = String.fromCharCode(questionOptions);
-        console.log(questionLetter + ": " + questionArray[position].answers[i]);
-      questionOptions ++;
+      console.log(questionLetter + ": " + questionArray[position].answers[i]);
+      questionOptions++;
     
     };
     givenAnswer = prompt("What's your answer?");
      
-};
+  };
 
-function checkAnswer() {
+  function checkAnswer() {
     console.log(typeof givenAnswer);
     console.log(typeof questionArray[position].correctAnswer);
     if (givenAnswer === questionArray[position].correctAnswer) {
-        score++;
-        console.log("Correct! \nScore is now: " + score);
+      score++;
+      console.log("Correct! \nScore is now: " + score);
     } else {
-        console.log("Wrong!\n Score is now: " + score);
+      console.log("Wrong!\n Score is now: " + score);
     }
-};
+  };
 
-while ("exit" !== givenAnswer) {
+  while ("exit" !== givenAnswer) {
     questionOptions = 65;
     randomizer();
     checkAnswer();
-};
+  };
+}) ();
